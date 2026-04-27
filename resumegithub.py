2dfe417abb37d2725db9b0e1f56a813f6a2a9853
dacarc <diegocarcamo92@gmail.com>
2026-04-27 10:33:39 -0400

    Remove pip install command from resumegithub.py
    Removed the pip install command for streamlit.

--- resumegithub.py
+++ resumegithub.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1whudTm0RAyj9av6tkJASW5nhGDXzQcld
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import plotly.express as px
